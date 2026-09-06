9e9296252f7656bda2ea7b61c1b61933b06c253b
Victor Ly <ly.victor2000@gmail.com>
2026-09-06 03:02:02 +0200

    Connect Web with Lib and refactor index page to dynamically display project categories

--- ZealandPortfolioWeb/Pages/Index.cshtml.cs
+++ ZealandPortfolioWeb/Pages/Index.cshtml.cs
@@ -1,12 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZealandPortfolioLib;
 
 namespace ZealandPortfolioWeb.Pages;
 
 public class IndexModel : PageModel
 {
+    /// <summary>
+    /// The repository for accessing project categories.
+    /// </summary>
+    private readonly IGenericPageRepository<ProjectCategory> _projectCategoryRepository;
+
+    /// <summary>
+    /// Gets or sets the list of project categories to be displayed on the index page.
+    /// </summary>
+    public List<ProjectCategory> ProjectCategories { get; set; } = new List<ProjectCategory>();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="IndexModel"/> class with the specified project category repository.
+    /// </summary>
+    /// <param name="projectCategoryRepository">The project category repository to use.</param>
+    public IndexModel(IGenericPageRepository<ProjectCategory> projectCategoryRepository)
+    {
+        _projectCategoryRepository = projectCategoryRepository;
+    }
+
     public void OnGet()
     {
-
+        ProjectCategories = _projectCategoryRepository.ReadAll();
     }
 }
--- ZealandPortfolioWeb/Program.cs
+++ ZealandPortfolioWeb/Program.cs
@@ -1,7 +1,17 @@
+using ZealandPortfolioLib;
+using ZealandPortfolioLib.Services;
+using ZealandPortfolioLib.Repositories;
+using ZealandPortfolioLib.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<ProjectCategoryService>();
+builder.Services.AddSingleton<ProjectService>();
+builder.Services.AddSingleton<IGenericPageRepository<ProjectCategory>, GenericPageRepository<ProjectCategory>>();
+builder.Services.AddSingleton<IGenericPageRepository<Project>, GenericPageRepository<Project>>();
 
 var app = builder.Build();
 
