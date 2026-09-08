90a873d92c6151d8cf23a6809c28e91c81c7b7c1
Victor Ly <ly.victor2000@gmail.com>
2026-09-08 12:49:30 +0200

    Production environment fix

--- ZealandPortfolioWeb/Program.cs
+++ ZealandPortfolioWeb/Program.cs
@@ -18,7 +18,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler();
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
