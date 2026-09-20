37263a0d9b06c196706a761b42c13c0aabcd5758
Victor Ly <ly.victor2000@gmail.com>
2026-09-20 17:38:31 +0200

    Add a `response code 418` easter egg page

---
+++ ZealandPortfolioWeb/Pages/Errors/ImATeapot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ZealandPortfolioWeb.Pages.Errors
+{
+    public class ImATeapotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
